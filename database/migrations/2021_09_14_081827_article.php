1f4ad6bd4d9d1c5ff3f3293e0e1d0f5fd2e4a58a
Lehien99 <56247237+Lehien99@users.noreply.github.com>
2021-09-15 00:24:55 +0700

    Initial commit

---
+++ database/migrations/2021_09_14_081827_article.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Article extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('article', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('Title');
+            $table->text('Summary');
+            $table->longText('Content');
+            $table->string('Image');
+            $table->string('IsPublisher');
+            $table->integer('Highlights');
+            $table->integer('View')->nullable();
+            $table->integer('idCategory')-> unsigned();
+            $table->foreign('idCategory')-> references('id')-> on('category');
+            $table->integer('idUser')-> unsigned();
+            $table->foreign('idUser')->references('id')-> on('user');
+            // $table->integer('idManager')-> unsigned();
+            // $table->foreign('idManager')->references('id')-> on('manager');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('article');
+    }
+}
